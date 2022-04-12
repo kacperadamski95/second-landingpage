66918291a9281a5db75114609161ab15076d9c3e
Kacper Adamski <kacper.adamski95@gmail.com>
2022-04-12 20:40:27 +0200

    created js file

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const menuIconHamburger = document.querySelector(".hamburger--icon");
+    const menuIconClose = document.querySelector(".close--icon");
+    const navigationMobile = document.querySelector(".navigation__links");
+    const mainPart = document.querySelector(".main--hidden");
+    const footerPart = document.querySelector(".footer--hidden");
+
+    menuIconHamburger.addEventListener("click", (event) => {
+        mainPart.classList.add("main--part");
+        footerPart.classList.add("main--part");
+        navigationMobile.classList.add("navigation--mobile--active");
+        menuIconHamburger.classList.add("menu--icon--hidden");
+    });
+
+    menuIconClose.addEventListener("click", (event) => {
+        navigationMobile.classList.remove("navigation--mobile--active");
+        menuIconHamburger.classList.remove("menu--icon--hidden");
+        mainPart.classList.remove("main--part");
+        footerPart.classList.remove("main--part");
+    });
+
+
+});
